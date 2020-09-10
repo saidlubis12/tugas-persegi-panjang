ba9ed13ec4568f3743e176352f5f1a3e60cb5046
ninjaa <alkhudrisaid83@gmail.com>
2020-09-10 19:49:40 +0700

    first commit

---
+++ persegip2.js
@@ -0,0 +1,13 @@
+function persegiPanjang(x1, a1) {
+  x1 = 4;
+  a1 = 5;
+
+  hasil = x1 * a1;
+
+  return hasil;
+}
+persegiPanjang(4, 5);
+document.write(`Menghitung volume persegi panjang<br>`);
+document.write(`Panjang : ${p}<br>`);
+document.write(`lebar : ${l}<br>`);
+document.write(`Luas Persegi Panjang : ${hasil} Perhitungan<br>`);
